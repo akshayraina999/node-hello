3378354af52846c30fb6cf627bf7f3ee4cd51a07
Akshay Raina <58293532+akshayraina999@users.noreply.github.com>
2024-04-02 16:11:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hey  there Node folks!\n'
+  const msg = 'Hey  there Node folkss!\n'
   res.end(msg);
 });
 
